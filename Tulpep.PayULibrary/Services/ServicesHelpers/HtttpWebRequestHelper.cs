6ed056ac4b108d5bb8cff0246912cd7436975a5c
Juan Carlos Hernandez Ramos <jc.hernandez129@uniandes.edu.co>
2016-06-28 11:43:49 -0500

    AddCertificateToRequest

--- Tulpep.PayULibrary/Services/ServicesHelpers/HtttpWebRequestHelper.cs
+++ Tulpep.PayULibrary/Services/ServicesHelpers/HtttpWebRequestHelper.cs
@@ -1,6 +1,8 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Net;
 using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Tulpep.PayULibrary.Services.ServicesHelpers
@@ -55,29 +57,65 @@ namespace Tulpep.PayULibrary.Services.ServicesHelpers
             /// <completionlist cref="SSL">
             /// Add here the SSL certificate 
             /// </completionlist>
-            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(
-               delegate { return true; }
-            );
-
-            req.ContentType = "application/json; charset=utf-8";
-            req.Accept = "application/json";
-            req.Headers[HttpRequestHeader.AcceptLanguage] = pLanguage;
-            string _cred = string.Format("{0} {1}", "Basic", base64);
-            req.Headers[HttpRequestHeader.Authorization] = _cred;
-            req.Method = httpMethod.ToString();
-
-            if ((httpMethod == HttpMethod.POST || httpMethod == HttpMethod.PUT) && requestJson != null)
+            X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            certStore.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection certCollection = certStore.Certificates.Find(
+                                       X509FindType.FindByThumbprint,
+                                       // Replace below with your cert's thumbprint
+                                       "6443733097b0de9dfb7a79519dd70773406ef16b",
+                                       false);
+            // Get the first cert with the thumbprint
+            if (certCollection.Count > 0)
             {
-                byte[] bytes = Encoding.UTF8.GetBytes(requestJson);
-                req.ContentLength = bytes.Length;
-                using (System.IO.Stream os = req.GetRequestStream())
-                {
-                    os.Write(bytes, 0, bytes.Length);
-                    os.Close();
-                }
-            }
+                X509Certificate2 cert = certCollection[0];
 
-            return (HttpWebResponse)req.GetResponse();
+                req.ClientCertificates.Add(cert);
+
+                certStore.Close();
+
+                req.ContentType = "application/json; charset=utf-8";
+                req.Accept = "application/json";
+                req.Headers[HttpRequestHeader.AcceptLanguage] = pLanguage;
+                string _cred = string.Format("{0} {1}", "Basic", base64);
+                req.Headers[HttpRequestHeader.Authorization] = _cred;
+                req.Method = httpMethod.ToString();
+
+                if ((httpMethod == HttpMethod.POST || httpMethod == HttpMethod.PUT) && requestJson != null)
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(requestJson);
+                    req.ContentLength = bytes.Length;
+                    using (System.IO.Stream os = req.GetRequestStream())
+                    {
+                        os.Write(bytes, 0, bytes.Length);
+                        os.Close();
+                    }
+                }
+                return (HttpWebResponse)req.GetResponse();
+            }
+            else
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(
+                    delegate { return true; }
+                    );
+                req.ContentType = "application/json; charset=utf-8";
+                req.Accept = "application/json";
+                req.Headers[HttpRequestHeader.AcceptLanguage] = pLanguage;
+                string _cred = string.Format("{0} {1}", "Basic", base64);
+                req.Headers[HttpRequestHeader.Authorization] = _cred;
+                req.Method = httpMethod.ToString();
+
+                if ((httpMethod == HttpMethod.POST || httpMethod == HttpMethod.PUT) && requestJson != null)
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(requestJson);
+                    req.ContentLength = bytes.Length;
+                    using (System.IO.Stream os = req.GetRequestStream())
+                    {
+                        os.Write(bytes, 0, bytes.Length);
+                        os.Close();
+                    }
+                }
+                return (HttpWebResponse)req.GetResponse();
+            }
         }
     }
 }
