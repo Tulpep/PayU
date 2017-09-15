8d921e3ef9b1a9a35e5027e2ccd3e6c60cd3c18c
Juan Carlos Hernandez Ramos <jc.hernandez129@uniandes.edu.co>
2017-09-15 18:02:27 -0500

    ValueTwoDecimalPlaces

---
+++ Tulpep.PayULibrary/Services/ServicesHelpers/MathHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tulpep.PayULibrary.Services.ServicesHelpers
+{
+    public static class MathHelper
+    { 
+        public static decimal RoundDecimal(decimal price)
+        {
+            decimal result = Math.Round(price, 2, MidpointRounding.ToEven);
+            return result;
+        }
+    }
+}
